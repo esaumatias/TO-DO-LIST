fe48e1fa0ef8ef4d5fd6e793feff8e3422b6a8fc
Esau Matias <esaumatias@10.0.0.109>
2022-06-24 13:49:51 -0300

    styled : implementa estilização para os cards das tasks

--- src/Components/CardsTasks/CardsTasks.js
+++ src/Components/CardsTasks/CardsTasks.js
@@ -1,27 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import AppContext from '../../Context/AppContext';
 import { Card, Row, Col, Spinner } from 'react-bootstrap';
 
 function CardsTasks() {
-  const { allTasks } = useContext(AppContext);
-
+    const { allTasks } = useContext(AppContext);
+    
     return (
       <>
         {allTasks.length > 0 ? (
-          <Row xs={1} md={5} className="g-2">
-          {allTasks.map((value, index) => (
-            <Col key={index}>
-              <Card bg="dark" text='light'>
-                <Card.Body>
-                  <Card.Title>{value.tasks}</Card.Title>
-                </Card.Body>
-                <Card.Footer style={{textAlign: "end"}}>
-                    {value.date}
-                </Card.Footer>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+            <Row xs={1} md={5} className="g-2">
+            {allTasks.map((value, index) => (
+                <Col key={index}>
+                <Card bg={value.status} text='dark'>
+                    <Card.Body>
+                    <Card.Title>{value.tasks}</Card.Title>
+                    </Card.Body>
+                    <Card.Footer style={{textAlign: "end"}}>
+                        {value.date}
+                    </Card.Footer>
+                </Card>
+                </Col>
+            ))}
+            </Row>
         ) : <Spinner animation="border" />}
       </>
     );
