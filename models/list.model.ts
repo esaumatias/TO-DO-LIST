c824a110fad622086310d0d0c334765374144909
Esau Matias <esaumatias@10.0.0.109>
2022-06-24 11:28:27 -0300

    feat: implementa connection com banco de dados mysql

---
+++ models/list.model.ts
@@ -0,0 +1,28 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import List from '../interfaces/list.interface';
+
+export default class ListModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<List[]> {
+    const result = await this.connection
+      .execute('SELECT * FROM tasks');
+    const [rows] = result;
+    return rows as List[];
+  }
+
+  public async create(list: List): Promise<List> {
+    const { tasks, date, status } = list;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO tasks (tasks, date, status) VALUES (?, ?, ?)',
+      [tasks, date, status],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...list };
+  }
+}
