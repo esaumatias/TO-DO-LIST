f828dbd3ae36f77c14739c53b346bff22ac3f557
Esau Matias <esaumatias@10.0.0.109>
2022-06-26 16:30:35 -0300

    test: implementa testes para camada de service

--- backend/tests/list.model.test.ts
+++
@@ -1,4 +0,0 @@
-import LitsModel from '../models/list.model';
-import Connection from '../models/connection';
-
-const litsModel = new LitsModel(Connection);
---
+++ backend/tests/list.service.test.ts
@@ -0,0 +1,52 @@
+import ListService from '../services/list.service';
+
+describe('Testando o List do Service', () => {
+
+    const listService = new ListService();
+
+    let task = {
+        title: 'TITLE TESTE',
+        tasks: 'imagina que existe alguma coisa escrita aqui',
+        date: '06-02-2022',
+        status: 'success',
+    }
+
+    let updateTask = {
+        title: 'UPDATE TITLE',
+        tasks: 'imagina que existe alguma coisa escrita aqui',
+        date: '06-02-2022',
+        status: 'warning',
+    }
+
+    it('Verifica se create consegue criar nova task', async () => {
+        const newTask = await listService.create(task)
+        expect(newTask).not.toBeInstanceOf(Error);
+        expect(newTask).toHaveProperty('id');
+        expect(newTask.title).toBe(task.title);
+    })
+
+    it('Verifica se getAll consegue buscar todas as Tasks', async () => {
+        const allTask = await listService.getAll();
+        expect(allTask).toBeDefined;
+    })
+
+    it('Verifica se getById consegue encontrar task por id', async () => {
+        const task = await listService.getById(1);
+        expect(task.id).toBe(1);
+    })
+
+    it('Verifica se update consegue modificar dados da task', async () => {
+        const newTask = await listService.update(1, updateTask);
+        const task = await listService.getById(1);
+        expect(newTask).not.toBeInstanceOf(Error);
+        expect(newTask.status).toBe(updateTask.status);
+        expect(task.title).toBe(updateTask.title);
+    })
+
+    it('Verifica se remove consegue apagar task por id', async () => {
+        const removeTask = await listService.remove(1);
+        const task = await listService.getById(1);
+        expect(removeTask).not.toBeInstanceOf(Error);
+        expect(task).not.toBeDefined;
+    })
+});
